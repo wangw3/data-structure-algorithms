05cb6fb6312bf43beeb1071c70bc76b667ad2601
wangw3 <931628503@qq.com>
2018-08-20 22:24:55 -0700

    add weave function

--- data-structure/src/queue.js
+++ data-structure/src/queue.js
@@ -3,6 +3,22 @@
  * remove -> array.pop()
  */
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+}
 
 export default Queue;
