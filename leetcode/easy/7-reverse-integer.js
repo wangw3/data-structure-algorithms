10dd302a60eb395fdc786b12e8a6e9ccb7894034
wangw3 <931628503@qq.com>
2018-08-26 14:01:48 -0700

    updated P7

--- leetcode/easy/7-reverse-integer.js
+++ leetcode/easy/7-reverse-integer.js
@@ -2,25 +2,36 @@
  * @param {number} x
  * @return {number}
  */
-// var reverse = function(x) {
-//     let solution;
-//     if(x < 0) {
-//         solution = String(x).split('').reverse();
-//         solution.pop();
-//         solution = 0 - Number(solution.join(''));
-//     }
-//     else {
-//         solution = Number(String(x).split('').reverse().join(''));
-//     }
-    
-//     return solution;
-// };
-
 var reverse = function(x) {
-    var res = 0;
-    while(x){
-        res = res*10 +x%10;
-        x = parseInt(x/10);  
+    let solution;
+    if(x < 0) {
+        solution = String(x).split('').reverse();
+        solution.pop();
+        solution = Number(solution.join('')) > Math.pow(2,31)? 0 : 0 - Number(solution.join(''));
     }
-    return res;
-}
+    else {
+        solution = Number(String(x).split('').reverse().join(''));
+        solution = solution > Math.pow(2,31) - 1 ? 0 : solution;
+    }
+    
+    return solution;
+};
+// var reverse = function(x) {
+//     const sign = x >= 0
+//     const abs = x * (sign ? 1 : -1)
+//     let ret = 0
+  
+//     // need ~~ to fix divided i cause this is js
+//     for (let i = abs; i > 0; i = ~~(i / 10)) {
+//       ret = ret * 10 + i % 10
+//     }
+  
+//     ret = ret * (sign ? 1 : -1)
+  
+//     // js has no 32-bit number type
+//     if (ret >= 2147483648 || ret < -2147483648) {
+//       return 0
+//     }
+  
+//     return ret
+//   };
